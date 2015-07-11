eae9c9d171d3448b14fcebeef9a2b80bc95f27a2
Rui Zhang <rui.zhang@mongodb.com>
2015-07-11 11:34:36 -0700

    use ms address

--- clusters/shard/update_run_config.sh
+++ clusters/shard/update_run_config.sh
@@ -2,6 +2,6 @@
 
 source setting.sh
 
-sed -i -- "s/%%P1%%/\"$SSHUSER@$p1\"/g" *.json
+sed -i -- "s/%%MS%%/\"$SSHUSER@$ms\"/g" *.json
 sed -i -- "s/%%MS_PRIVATE_IP%%/$ms_private_ip/g" *.json
 sed -i -- "s/%%CLIENT%%/\"$SSHUSER@$mc\"/g" *.json
