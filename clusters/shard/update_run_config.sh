8784f5c505e3aab2afcec0decf0c98b1fe184194
Rui Zhang <rui.zhang@mongodb.com>
2015-07-10 20:44:02 -0700

    fix string replace for up address

--- clusters/shard/update_run_config.sh
+++ clusters/shard/update_run_config.sh
@@ -3,5 +3,5 @@
 source setting.sh
 
 sed -i -- "s/%%P1%%/\"$SSHUSER@$p1\"/g" *.json
-sed -i -- "s/%%MS_PRIVATE_IP%%/\"$SSHUSER@$ms_private_ip\"/g" *.json
+sed -i -- "s/%%MS_PRIVATE_IP%%/$ms_private_ip/g" *.json
 sed -i -- "s/%%CLIENT%%/\"$SSHUSER@$mc\"/g" *.json
